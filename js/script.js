79b7ce431022d0eb35df3cfd82f0c09282235b8a
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-03 15:18:36 +0100

    add names script

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 "use strict";
 
+// let namesFemale = ['Asia', 'Kasia', 'Ola', 'Jola'];
+// let namesMale = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+// let namesAll = namesFemale.concat(namesMale);
+
+// function addName(newName) {
+//   if (namesAll.includes(newName) == false) {
+//     return namesAll.push(newName);    
+//   } else {
+//     return console.log('Imię już istnieje');
+//   }
+// }
+
 let namesFemale = ['Asia', 'Kasia', 'Ola', 'Jola'];
 let namesMale = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 let namesAll = namesFemale.concat(namesMale);
 
-let newName = 'Marian';
+let nameNew = prompt('Podaj imię');
 
-if (namesAll.indexOf(newName) === -1) {
-  namesAll.push(newName);
+if (namesAll.indexOf(nameNew) === -1) {
+  namesAll.push(nameNew);
 } else {
-  console.log('Imię już istnieje');
+  alert('Imię już istnieje');
 }
